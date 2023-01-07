9e0ee7a00e3863d43079def38abfefe475a361d6
anthony-langham <anthony.langham@gmail.com>
2023-01-07 15:48:35 +0000

    added prettier & created js and added jquery fx

---
+++ script.js
@@ -0,0 +1,40 @@
+'use strict';
+
+// create an array of themes
+
+const themes = [];
+themes[0] = 'css/themes/green-white.css';
+themes[1] = 'css/themes/grey-white.css';
+themes[2] = 'css/themes/indigo-white.css';
+themes[3] = 'css/themes/red-white.css';
+themes[4] = 'css/themes/white-blue.css';
+themes[5] = 'css/themes/white-grey.css';
+themes[6] = 'css/themes/white-indigo.css';
+themes[7] = 'css/themes/white-red.css';
+themes[8] = 'css/themes/yellow-black.css';
+
+//stack overflow example function
+
+// $(function() {
+//     var style = link[Math.floor(Math.random() * link.length )];
+//     if (document.createStyleSheet){
+//         document.createStyleSheet(style);
+//     }else{
+//         $('<link />',{
+//             rel :'stylesheet',
+//             type:'text/css',
+//             href: style
+//         }).appendTo('head');
+//     }
+// });
+
+// This is a JQuery function, needed to import JQuery in the html file - review this **
+
+$(function () {
+  let style = themes[Math.floor(Math.random() * themes.length)];
+  $('<link />', {
+    rel: 'stylesheet',
+    // type: 'text/css',
+    href: style,
+  }).appendTo('head');
+});
